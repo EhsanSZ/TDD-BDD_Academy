1acae149785ab6aeda2282cdb6fb9bc2064b5918
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-09-08 23:28:45 +0430

    TDD FizzBuzz

--- FizzBuzz/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzzTests.cs
@@ -27,38 +27,5 @@ namespace FizzBuzz
             Assert.Equal(expected, actual[element]);
         }
 
-
-        //[Fact]
-        //public void Start_ShouldReturnAListWithProperFizzValues()
-        //{
-        //    const int rounds = 100;
-
-        //    var actual = FizzBuzz.Start(rounds);
-
-        //    Assert.Equal("Fizz", actual[2]);
-        //    Assert.Equal("Fizz", actual[8]);
-        //}
-
-        //[Fact]
-        //public void Start_ShouldReturnAListWithProperBuzzValues()
-        //{
-        //    const int rounds = 100;
-
-        //    var actual = FizzBuzz.Start(rounds);
-
-        //    Assert.Equal("Buzz", actual[4]);
-        //    Assert.Equal("Buzz", actual[9]);
-        //}
-
-        //[Fact]
-        //public void Start_ShouldReturnAListWithProperFizzBuzzValues()
-        //{
-        //    const int rounds = 100;
-
-        //    var actual = FizzBuzz.Start(rounds);
-
-        //    Assert.Equal("FizzBuzz", actual[14]);
-        //    Assert.Equal("FizzBuzz", actual[29]);
-        //}
     }
 }
---
+++ FizzBuzz/TDD_FizzBuzzTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz
+{
+    public class TDD_FizzBuzzTest
+    {
+        [Theory]
+        [InlineData("Fizz",2)]
+        [InlineData("Buzz",4)]
+        [InlineData("FizzBuzz",14)]
+        public void Start_ShouldReturnProperValueAtExpectedElement(string expected, int element)
+        {
+            const int input = 120;
+
+            var actual = TDD_FizzBuzz.Start(input);
+
+            Assert.Equal(expected, actual[element]);
+
+        }
+
+        [Fact]
+        public void Start_ShouldReturnAListWithGivenRoundLenth()
+        {
+            const int input = 120;
+
+            var actual = TDD_FizzBuzz.Start(input);
+
+            Assert.Equal(input, actual.Count);
+        }
+
+        //[Fact]
+        //public void Start_ShouldReturnProperValueAtExpectedIndex_WhenNumberIsDividebleTo3()
+        //{
+        //    const int input = 120;
+        //    const int element = 2;
+
+        //    var actual = TDD_FizzBuzz.Start(input);
+
+        //    Assert.Equal("Fizz", actual[element]);
+        //}
+
+        //[Fact]
+        //public void Start_ShouldReturnProperValueAtExpectedIndex_WhenNumberIsDividebleTo5()
+        //{
+        //    const int input = 120;
+        //    const int element = 4;
+
+        //    var actual = TDD_FizzBuzz.Start(input);
+
+        //    Assert.Equal("Buzz", actual[element]);
+        //}
+
+        //[Fact]
+        //public void Start_ShouldReturnProperValueAtExpectedIndex_WhenNumberIsDividebleTo15()
+        //{
+        //    const int input = 120;
+        //    const int element = 14;
+
+        //    var actual = TDD_FizzBuzz.Start(input);
+
+        //    Assert.Equal("FizzBuzz", actual[element]);
+        //}
+
+    }
+}
