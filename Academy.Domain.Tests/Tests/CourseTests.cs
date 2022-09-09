ce9fa56b7c6e445b6a2c5e9f6840d81a9c76250e
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-09-09 22:44:54 +0430

    Course Domain

---
+++ Academy.Domain.Tests/Tests/CourseTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Xunit;
+
+namespace Academy.Domain.Tests.Tests
+{
+    public class CourseTests
+    {
+        [Fact]
+        public void Constructor_ShouldConstructCourseProperly()
+        {
+            const int id = 1;
+            const string name = "tdd & bdd";
+            const bool isOnline = true;
+            const double tuition = 600;
+            const string instructor = "hossein";
+
+            var course = new Course( id , name , isOnline , tuition , instructor);
+
+            Assert.Equal(id, course.Id);
+            Assert.Equal(name, course.Name);
+            Assert.Equal(isOnline, course.IsOnline);
+            Assert.Equal(tuition, course.Tuition);
+            Assert.Equal(instructor, course.Instructor);
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowException_WhenNameIsNotProvided()
+        {
+            const int id = 1;
+            const string name = "";
+            const bool isOnline = true;
+            const double tuition = 240;
+            const string instructor = "hossein";
+
+            void course() => new Course(id, name, isOnline, tuition, instructor);
+
+            Assert.Throws<Exception>(course);
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowException_WhenTuitionIsNotProvided()
+        {
+            const int id = 1;
+            const string name = "tdd & bdd";
+            const bool isOnline = true;
+            const double tuition = 0;
+            const string instructor = "hossein";
+
+            void course() => new Course(id, name, isOnline, tuition, instructor);
+
+            Assert.Throws<Exception>(course);
+        }
+    }
+
+}
---
+++ Academy.Domain/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Academy.Domain
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOnline { get; set; }
+        public double Tuition { get; set; }
+        public string Instructor { get; set; }
+
+        public Course(int id, string name, bool isOnline, double tuition, string instructor)
+        {
+            GuardAgainstInvalidName(name);
+            GuardAgainstInvalidTuition(tuition);
+
+            Id = id;
+            Name = name;
+            IsOnline = isOnline;
+            Tuition = tuition;
+            Instructor = instructor;
+        }
+
+        private static void GuardAgainstInvalidName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new Exception();
+        }
+
+        private static void GuardAgainstInvalidTuition(double tuition)
+        {
+            if (tuition <= 0)
+                throw new Exception();
+        }
+    }
+}
