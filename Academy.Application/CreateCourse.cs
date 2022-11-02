f866457734a730d4dbf704795d34e3e824c34195
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-11-02 19:23:31 +0330

    Acceptance Tests

--- Academy.Application/CreateCourse.cs
+++ Academy.Application/CreateCourse.cs
@@ -2,6 +2,7 @@ namespace Academy.Application
 {
     public class CreateCourse
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public bool IsOnline { get; set; }
         public double Tuition { get; set; }
